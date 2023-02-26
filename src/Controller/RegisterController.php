ea8688b6bcb23f821205c439c46d47dde96ff647
vidal <nonovidal78@gmail.com.com>
2023-02-26 23:52:05 +0100

    problème résolu

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -12,7 +12,8 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\Persistence\ManagerRegistry;
-
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasher;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class RegisterController extends AbstractController
 {
@@ -23,17 +24,19 @@ class RegisterController extends AbstractController
     }*/
 
     #[Route('/inscription', name: 'app_register')]
-    public function index(Request $request, EntityManagerInterface $entity): Response
+    public function index(Request $request, EntityManagerInterface $entity, UserPasswordHasherInterface $passwordHasher): Response
     {
 
         $user = new User();
         $form = $this->createForm(RegisterType::class, $user);
+        
 
         $form -> handleRequest($request);
 
         if($form->isSubmitted() && $form->isValid()){
             //$user = $form->getData();
-            
+            $hashedPassword = $passwordHasher->hashPassword($user, $user->getPassword());
+            $user->setPassword($hashedPassword);
             $entity->persist($user);
             $entity->flush();
             
--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -2,12 +2,14 @@
 
 namespace App\Security;
 
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Security;
+use Symfony\Component\Security\Csrf\CsrfTokenManagerInterface;
 use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
@@ -20,6 +22,8 @@ class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
     use TargetPathTrait;
 
     public const LOGIN_ROUTE = 'app_login';
+
+    
 
     public function __construct(private UrlGeneratorInterface $urlGenerator)
     {
